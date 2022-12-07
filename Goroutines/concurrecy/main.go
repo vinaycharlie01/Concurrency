455b8d8d1c1b94595b816ca3f4caab667e74f162
vinaycharlie01 <vinaykumaree5@gmail.com>
2022-12-07 16:15:46 +0530

    m

--- Goroutines/concurrecy/main.go
+++ Goroutines/concurrecy/main.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func DringkingCoffee(a string) {
+func DringkingCoffee(a string, wg *sync.WaitGroup) {
+	//after completing loop we need to tell the waiting its done
+	defer wg.Done()
 	for i := 0; i < 3; i++ {
 		fmt.Printf("Dringking coffee,index %d and name %s\n", i, a)
+		time.Sleep(1 * time.Millisecond)
 	}
 }
 
-func Watching_Tv(a string) {
+func Watching_Tv(a string, wg *sync.WaitGroup) {
+	//wg.Done()
 	for i := 0; i < 3; i++ {
 		fmt.Printf("Waching tv ,index %d and name %s\n", i, a)
+		time.Sleep(1 * time.Millisecond)
 
 	}
+	wg.Done()
 }
 
 func main() {
 	var a string = "Vinay"
-	DringkingCoffee(a)
-	Watching_Tv(a)
+	var wg sync.WaitGroup
+
+	//How many go routines running
+	wg.Add(2)
+	go DringkingCoffee(a, &wg)
+	go Watching_Tv(a, &wg)
+	//time.Sleep(2 * time.Second)
+	//then main() function waiting for go routines function  we need to wait for go routines function joined to the main function
+	wg.Wait()
 
 }
